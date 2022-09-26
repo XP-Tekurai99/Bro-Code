e3c7c7598ebf55eb3e217419dbb05b04680794d0
Taeshawn Threatt <110959584+XP-Tekurai99@users.noreply.github.com>
2022-09-25 22:24:42 -0700

    Create MathFunctions.js

---
+++ JavaScript/MathFunctions.js
@@ -0,0 +1,19 @@
+// Math Functions
+
+let x = 3.14;
+let y = 7
+let z = 9
+let max;
+let min;
+
+// x = Math.round(x);
+// x = Math.floor(x);
+// x = Math.ceiling(x);
+// x = Math.pow(x, 2);
+// x = Math.sqrt(x);
+// x = Math.abs(x);
+max = Math.max(x, y, z);
+min = Math.min(x, y, z);
+// x = Math.PI;
+
+console.log(max, min);
