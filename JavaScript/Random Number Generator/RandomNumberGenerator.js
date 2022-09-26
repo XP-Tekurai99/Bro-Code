4a439192fa51e62b45cb380e36e40b6ee894f77f
Taeshawn Threatt <110959584+XP-Tekurai99@users.noreply.github.com>
2022-09-25 22:37:57 -0700

    Create RandomNumberGenerator.js

---
+++ JavaScript/Random Number Generator/RandomNumberGenerator.js
@@ -0,0 +1,16 @@
+// Random Number Generator
+
+let x;
+let y;
+let z;
+
+document.getElementById("rollButton").oneclick = function()
+{
+    x = Math.floor(Math.random() * 20) + 1;
+    y = Math.floor(Math.random() * 20) + 1;
+    z = Math.floor(Math.random() * 20) + 1;
+
+    document.getElementById("xLabel").innerHTML = x;
+    document.getElementById("yLabel").innerHTML = y;
+    document.getElementById("zLabel").innerHTML = z;
+}
